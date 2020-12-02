d6c8724dcc780a705cd7ba3c0fabd5453b50c572
khanhafe <khanhafe@gmail.com>
2020-12-02 10:51:08 +0530

    day1 day 1 changes for redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import Reducer from "./store/reducer";
 
 import App from "./App";
+
+const store = createStore(Reducer);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
---
+++ src/store/reducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+  posts: [],
+  loginModal: {
+    open: false
+  }
+};
+
+const reducer = (state = initialState, action) => {
+  // if (action.type === "ADD_POST") {
+  //   state.posts.push(action.payload);
+  // }
+
+  return state;
+};
+
+export default reducer;
